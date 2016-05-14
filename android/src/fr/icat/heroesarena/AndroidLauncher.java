3189e5f4c655b22007134d0d2d55cb50055f83f5
icat-studiofr <icat.studiofr@gmail.com>
2016-05-14 19:14:11 +0200

    first

---
+++ android/src/fr/icat/heroesarena/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fr.icat.heroesarena;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fr.icat.heroesarena.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
